59ef47c96792e2dbdd07665ad603486d4781c66c
elajasho <elaja.sho@gmail.com>
2023-11-20 22:28:13 +0300

    added SelenideStepsTest

---
+++ src/test/java/tests/SelenideStepsTest.java
@@ -0,0 +1,46 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.Allure;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import pages.RegistrationFormPage;
+import pages.components.TableComponent;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
+import static org.openqa.selenium.By.linkText;
+
+@Tag("SelenideStepsTest")
+public class SelenideStepsTest extends TestBase {
+
+
+            private final static String REPOSITORY = "eroshenkoam/allure-example";
+            private final static String ISSUE = "Issue_created_to_test_allure_reports";
+
+            @Test
+            public void testIssueSearch() {
+                SelenideLogger.addListener("allure", new AllureSelenide());
+                Allure.step("Open github mainpage", () -> {
+                    open("https://github.com");
+                });
+                Allure.step("Search repo " + REPOSITORY, () -> {
+                    $("[data-target='qbsearch-input.inputButtonText']").click();
+                    $("#query-builder-test").setValue(REPOSITORY).pressEnter();
+                });
+                Allure.step("Click link " + REPOSITORY, () -> {
+                    $(linkText(REPOSITORY)).click();
+                });
+                Allure.step("Click Tab Issues", () -> {
+                    $("#issues-tab").click();
+                });
+                Allure.step("Check that issue  " + ISSUE + " exist", () -> {
+                    $(withText(ISSUE)).should(Condition.exist);
+                });
+            }
+        }
